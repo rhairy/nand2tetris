9ab4f1c780d737d9426a0ceb4f114397f9f19d58
rhairy <rhairy@outlook.com>
2017-05-03 15:38:45 -0700

    Added Assembly program to multiply.

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+(BEGIN)
+	// Assign value of R0 to count.
+	@R0
+	D=M
+	@count
+	M=D
+	
+	// Initialize R2 to 0.
+	@0
+	D=A
+	@R2
+	M=D
+(LOOP)
+	@count
+	D=M
+	@END
+	D;JEQ
+	@R1
+	D=M
+	@R2
+	M = M + D
+	@count
+	M=M-1
+	D=M
+	@LOOP
+	D;JGT
+(END)
